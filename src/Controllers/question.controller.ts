2e1e41c380d5530baa7c4582ada681d8c3738eb4
Alamin <alaminshaheen@iut-dhaka.edu>
2021-09-15 00:46:46 +0600

    added single question API route

--- src/Controllers/question.controller.ts
+++ src/Controllers/question.controller.ts
@@ -7,12 +7,24 @@ const create = async (req: Request, res: Response, next: NextFunction) => {
 	logging.info("Attempting to create new question");
 	const { Title, Description, CreatedBy, CreatedAt }: IQuestion = req.body;
 
-	if (!Title || !CreatedBy || !Description || typeof Description !== "string" || typeof Title !== "string" || typeof CreatedBy !== "string") {
+	if (
+		!Title ||
+		!CreatedBy ||
+		!Description ||
+		typeof Description !== "string" ||
+		typeof Title !== "string" ||
+		typeof CreatedBy !== "string"
+	) {
 		return res.status(400).json({ message: "Invalid request" });
 	}
 
 	try {
-		const question = await Question.create({ Title, Description, CreatedBy, Answers: [] });
+		const question = await Question.create({
+			Title,
+			Description,
+			CreatedBy,
+			Answers: [],
+		});
 		logging.info(`New Question ${question._id} created`);
 		return res.status(201).json(question);
 	} catch (error) {
@@ -21,7 +33,11 @@ const create = async (req: Request, res: Response, next: NextFunction) => {
 	}
 };
 
-const getQuestions = async (req: Request, res: Response, next: NextFunction) => {
+const getQuestions = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
 	logging.info(`Incoming get request for all questions`);
 	try {
 		const questions = await Question.find().exec();
@@ -32,9 +48,22 @@ const getQuestions = async (req: Request, res: Response, next: NextFunction) => 
 	}
 };
 
+const getQuestion = async (req: Request, res: Response, next: NextFunction) => {
+	const questionId = req.params.questionId;
+	logging.info(`Incoming get request for questions ${questionId}`);
+	try {
+		const question = await Question.findById(questionId);
+		return res.status(200).json({ question });
+	} catch (error) {
+		logging.error(error);
+		return res.status(500).json({ error });
+	}
+};
+
 const questionController = {
 	create,
-	getQuestions
+	getQuestions,
+	getQuestion
 };
 
 export default questionController;
